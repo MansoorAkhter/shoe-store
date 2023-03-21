840f4b160f660a30146559f8e6f59b34860938d7
Mansoor Akhter <mansoor21st@Gmail.com>
2023-03-21 12:18:35 +0500

    dynamic carousel banner Done

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,14 @@
+import HeroBanner from "@/components/HeroBanner";
+import Wrapper from "@/components/Wrapper";
 
 export default function Home() {
-  return <main className="h-[2000px]">
-    HOME....
-  </main>
+  return (
+    <main>
+      <HeroBanner />
+
+      <Wrapper>
+
+      </Wrapper>
+    </main>
+  )
 }
